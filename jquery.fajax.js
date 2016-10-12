01dc7fcf8ecaac1831f94418917f181459552494
mik <mikail.aziev@gmail.com>
2016-10-12 14:39:18 +0300

    upload plugin

---
+++ jquery.fajax.js
@@ -0,0 +1,46 @@
+;(function ($) {
+
+    $.fn.fajax = function (options) {
+
+        var settings = $.extend({
+            beforeSend: function(){},
+            success: function(){},
+            error: function(){},
+            complete: function(){},
+        }, options);
+
+        return this.each(function() {
+
+            $(this).on('submit', function(e){
+
+                e.preventDefault();
+
+                var action = $(this).attr('action');
+                var method = $(this).attr('method');
+                
+                var data = $(this).serialize();
+
+                $.ajax({
+                    url: action,
+                    method: method,
+                    beforeSend: function(){
+                        settings.beforeSend();
+                    },
+                    success: function(){
+                        settings.success();
+                    },
+                    error: function(){
+                        settings.error();
+                    },
+                    complete: function(){
+                        settings.complete();
+                    },
+                });
+
+            });
+
+        });
+
+    };
+
+})(jQuery);

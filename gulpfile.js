948b5e46dc913081a60376bb7174a6abe3fe71ad
mik <mikail.aziev@gmail.com>
2016-10-16 16:36:49 +0300

    add minified version

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('compress', function(){
+    return gulp.src('jquery.fajax.js')
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min',
+        }))
+        .pipe(gulp.dest('.'));
+});

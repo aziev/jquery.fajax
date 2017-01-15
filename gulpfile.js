fd369ecab41670d537ea758b8c199ac9c2aebfcf
Mikail <mikail@ant.ms>
2017-01-15 21:06:19 +0300

    create dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 gulp.task('compress', function(){
-    return gulp.src('jquery.fajax.js')
+    return gulp.src('dist/jquery.fajax.js')
         .pipe(gulp.dest('demo'))
         .pipe(uglify())
         .pipe(rename({
             suffix: '.min',
         }))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('dist'));
 });
